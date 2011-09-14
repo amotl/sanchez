0302a9f678a31a7fe10024e0a28bc500faba47e4
Andreas Motl <amotl@vz.net>
2011-09-14 03:49:24 +0200

    - fixed buildout sandbox regarding dependencies - included pynids-0.6.2.tar.gz locally

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup (
             ]),
     install_requires = [
         'setuptools',
-        #'pynids==0.6.1',   # install manually, see INSTALL.rst
+        'pynids>=0.6.1',
         'dpkt==1.7',
     ],
     zip_safe = False,
