b63d3d119122b5a5fea34542f665ba5a6d6b6bf0
Andreas Motl <amotl@vz.net>
2011-10-01 01:42:56 +0200

    expand list of multiple headers in header-pretty-printer

--- src/sanchez/monkey.py
+++ src/sanchez/monkey.py
@@ -1,4 +1,5 @@
 import dpkt
+import types
 
 def pack_hdr(self):
     return ''.join([ '%s: %s\r\n' % t for t in self.headers.iteritems() ])
@@ -6,8 +7,12 @@ def pack_hdr(self):
 def pack_hdr_pretty(self):
     headers_list = []
     for name, value in self.headers.iteritems():
-        entry = '%s: %s' % (name.title(), value)
-        headers_list.append(entry)
+        # for multiple headers with same name, handle lists as well
+        if not type(value) is types.ListType:
+            value = [value]
+        for item in value:
+            entry = '%s: %s' % (name.title(), item)
+            headers_list.append(entry)
     headers_list.sort()
     headers_string = '\r\n'.join(headers_list) + '\r\n'
     return headers_string
